531a3fa74bed3f0fa5e7ff8f3c4f47cdba08772c
TrustyTraitor <35874258+TrustyTraitor@users.noreply.github.com>
2022-12-08 13:57:26 -0500

    Update ArrayUtils.hpp

--- utilities/Array/ArrayUtils.hpp
+++ utilities/Array/ArrayUtils.hpp
@@ -113,9 +113,9 @@ namespace ArrayUtils {
 
     // Prints array by running through it
     void printArray(int* arr, int size) {
-        for(int i = 0; i < size; i++)
-            {
-				std::cout << arr[i] << " ";}
+        for(int i = 0; i < size; i++) {
+		std::cout << arr[i] << " ";
+	}
         std::cout << std::endl;
     }
 }
